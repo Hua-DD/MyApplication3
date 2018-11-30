8e2536d0549d94a1246a2a2309cd51b26114dc71
hua <728502583@qq.com>
2018-11-30 21:13:19 +0800

    hhuahh

---
+++ app/src/main/java/com/example/hua/myapplication3/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.hua.myapplication3;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initView();
+    }
+
+    private void initView() {
+    int hua=666;
+    }
+}
